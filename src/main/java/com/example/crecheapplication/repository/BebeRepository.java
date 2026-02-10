91345a14438dfc402ffa9027ce98f52881347bc0
TEYEB Wissem <wissem.teyeb@supcom.tn>
2026-02-10 19:27:31 +0100

    Spring Boot project

---
+++ src/main/java/com/example/crecheapplication/repository/BebeRepository.java
@@ -0,0 +1,15 @@
+package com.example.crecheapplication.repository;
+
+import com.example.crecheapplication.model.Bebe;
+import com.example.crecheapplication.model.Parent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+@Repository
+public interface BebeRepository extends JpaRepository<Bebe,Long> {
+    Optional<Bebe> findById(Long id);
+    Optional<List<Bebe>> findByParentId(Long parentId);
+
+}
